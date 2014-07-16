b047987628b875e61e58e4163178f10d9a04dfa2
Clayton G. Hobbs <clay@lakeserv.net>
2014-07-16 17:03:49 -0400

    Document the client code.

--- client/polywar.js
+++ client/polywar.js
@@ -54,31 +54,42 @@ function Player(fill, stroke) {
 }
 
 Player.prototype.draw = function(ctx) {
+    // Save the state because we're gonna be doing transformations
     ctx.save();
 
+    // Set style
     ctx.fillStyle = this.fill;
     ctx.strokeStyle = this.stroke;
     ctx.lineWidth = 2;
 
+    // Transform according to the location and rotation of the player
     ctx.translate(this.pos[0], this.pos[1]);
     ctx.rotate(this.angle * Math.PI/128);
 
+    // Draw the triangle
     ctx.beginPath();
     ctx.moveTo(0, -15);
     ctx.lineTo(10, 10);
     ctx.lineTo(-10, 10);
+    // At this point, we can fill
     ctx.fill();
+    // Redraw the first two points so the outline will be complete and pointy
     ctx.lineTo(0, -15);
     ctx.lineTo(10, 10);
     ctx.stroke();
 
+    // Undo our transformations
     ctx.restore();
 }
 
+// Connect to the server
 var server = new WebSocket("ws://" + window.location.host + "/polywar-server");
+// Players will be stored here
 var players = {};
+// The ID of the guy playing on this client will be stored here
 var my_id;
 
+// Convenience function to send a packet with an opcode and an array of bytes
 WebSocket.prototype.sendPacket = function(opcode, bytes) {
     var msg = new ArrayBuffer(bytes.length + 1);
     var data = new Uint8Array(msg, 0, bytes.length + 1);
@@ -87,20 +98,24 @@ WebSocket.prototype.sendPacket = function(opcode, bytes) {
     server.send(msg);
 }
 
+// Send the introduction packet when the connection is complete
 server.onopen = function(event) {
     server.sendPacket(0x00, []);
 }
 
+// When we get a message from the server, react accordingly
 server.onmessage = function drawGame(event) {
     var reader = new FileReader();
     reader.addEventListener("loadend", function() {
         // reader.result contains the contents of blob as a typed array
         var msg = reader.result;
+        // If somehow a 0 byte message was sent, ignore it
         if (msg.byteLength === 0) {
             return;
         }
         var opcode = new Uint8Array(msg, 0, 1);
         switch (opcode[0]) {
+            // New player information
             case 0x00:
                 var id = (new Uint8Array(msg, 1, 1))[0];
                 var fill = new Uint8Array(msg, 2, 3);
@@ -110,10 +125,12 @@ server.onmessage = function drawGame(event) {
                     console.log(my_id);
                 players[id] = new Player(fill, stroke);
                 break;
+            // Player disconnected
             case 0x01:
                 var id = (new Uint8Array(msg, 1, 1))[0];
                 delete players[id];
                 break;
+            // Player update(s)
             case 0x02:
                 var offset = 1;
                 var id, vars, position;
@@ -143,15 +160,22 @@ server.onmessage = function drawGame(event) {
                 break;
         }
 
+        // Redraw
+        // TODO: don't make this happen every time we got a message from the
+        // server
+        // Get the canvas and a context for it
         var c = document.getElementById("polywarCanvas");
         var ctx = c.getContext("2d");
 
+        // Clear the canvas
         c.width = c.width;
+        // Draw all the players but me
         for (var p in players) {
             if (p == my_id)
                 continue;
             players[p].draw(ctx);
         }
+        // Draw myself
         players[my_id].draw(ctx);
     });
     reader.readAsArrayBuffer(event.data);
