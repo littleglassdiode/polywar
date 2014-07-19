a33f7617bef9d5f95b0d3c1304aa0a1252ed0738
Clayton G. Hobbs <clay@lakeserv.net>
2014-07-19 18:08:40 -0400

    Sped up player rotation again
    Half steps were a bad idea, since the client didn't know about them
    and they transferred into shots.  It would be possible for a shot to
    end up pretty far away from where the client thought it was.

--- server/variables.js
+++ server/variables.js
@@ -1,6 +1,6 @@
 exports.PLAYER_SPEED = 2;
 exports.PLAYER_REVERSE_SPEED = -2;
-exports.PLAYER_ROTATION_SPEED = 0.5;
+exports.PLAYER_ROTATION_SPEED = 1;
 exports.PLAYER_MAX_SHOTS = 5;
 
 exports.SHOT_SPEED = 6;
