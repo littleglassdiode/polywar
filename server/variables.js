0e7b038d539a3ee170137639b010fc6090a77339
Clayton G. Hobbs <clay@lakeserv.net>
2014-08-18 10:46:50 -0400

    Changed variables to be more BZFlag-like.
    Polywar is, after all, a 2D ripoff of BZFlag.

--- server/variables.js
+++ server/variables.js
@@ -1,7 +1,7 @@
 exports.PLAYER_SPEED = 2;
-exports.PLAYER_REVERSE_SPEED = -2;
+exports.PLAYER_REVERSE_SPEED = -1;
 exports.PLAYER_ROTATION_SPEED = 1;
 exports.PLAYER_MAX_SHOTS = 5;
 
-exports.SHOT_SPEED = 6;
-exports.SHOT_TIME = 120;
+exports.SHOT_SPEED = 8;
+exports.SHOT_TIME = 210;
